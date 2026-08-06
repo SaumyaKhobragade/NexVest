c2c77b607537bfd045d6f30d66956f4852e3858a
Saumya Khobragade <khobragadesaumya@gmail.com>
2026-08-06 18:21:22 +0530

    Add Education, Pricing and Stats Section

--- frontend/src/landing_page/home/Education.jsx
+++ frontend/src/landing_page/home/Education.jsx
@@ -1,7 +1,22 @@
 function Education() {
     return ( 
         <>
-            <h1>Education</h1>
+            <div className="container mt-5">
+                <div className="row">
+                    <div className="col">
+                        <img src="assets/education.svg" alt="Education" style={{width: "70%"}} />
+                    </div>
+                    <div className="col">
+                        <h1 className="mb-3 fs-2">Free and Open Market Education</h1>
+
+                        <p className="mt-5">Varsity is our free online stock market learning platform. It offers comprehensive courses for beginners and advanced traders alike.</p>
+                        <a href="" style={{ textDecoration: "none" }}>Varsity<i className="fas fa-arrow-right ml-2"></i></a>
+
+                        <p className="mt-5">TradingQ&A, the most active community of traders and investors in India for all your market questions.</p>
+                        <a href="" style={{ textDecoration: "none" }}>TradingQ&A<i className="fas fa-arrow-right ml-2"></i></a>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
--- frontend/src/landing_page/home/Pricing.jsx
+++ frontend/src/landing_page/home/Pricing.jsx
@@ -1,7 +1,28 @@
 function Pricing() {
     return ( 
         <>
-            <h1>Pricing</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col-4">
+                        <h1 className="mb-3 fs-2">Unbeatable Pricing</h1>
+                        <p>We pioneer transparent pricing with no hidden fees. Flat fees and no hidden costs.</p>
+                        <a href="" style={{ textDecoration: "none" }}>See Pricing<i className="fas fa-arrow-right ml-2"></i></a>
+                    </div>
+                    <div className="col-2"></div>
+                    <div className="col-6 mb-5">
+                        <div className="row text-center">
+                            <div className="col border p-3">
+                                <h1 className="mb-3">₹0</h1>
+                                <p>Free equity delivery and <br /> direct mutual funds</p>
+                            </div>
+                            <div className="col border p-3">
+                                <h1 className="mb-3">₹20</h1>
+                                <p>Flat fee for intraday <br /> and F&O trades</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
