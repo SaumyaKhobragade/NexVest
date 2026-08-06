274bd27978300daabfb0158aab7a101d07b5bbd0
Saumya Khobragade <khobragadesaumya@gmail.com>
2026-08-06 18:58:03 +0530

    Add Reusable Components - Open Account, Footer and Navbar

--- frontend/src/landing_page/Navbar.jsx
+++ frontend/src/landing_page/Navbar.jsx
@@ -1,7 +1,50 @@
 function Navbar() {
-    return ( 
+    return (
         <>
-            <h1>Navbar</h1>
+            <nav
+                class="navbar navbar-expand-lg border-bottom"
+                style={{ backgroundColor: "#FFF" }}
+            >
+                <div class="container p-2">
+                    <a class="navbar-brand" href="#">
+                        <img
+                            src="assets/logo.svg"
+                            style={{ width: "25%" }}
+                            alt="Logo"
+                        />
+                    </a>
+
+                    <form class="d-flex flex-row-reverse" role="search">
+                        <ul class="navbar-nav mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">
+                                    Signup
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">
+                                    About
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">
+                                    Product
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">
+                                    Pricing
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">
+                                    Support
+                                </a>
+                            </li>
+                        </ul>
+                    </form>
+                </div>
+            </nav>
         </>
     );
 }
--- frontend/src/landing_page/OpenAccount.jsx
+++ frontend/src/landing_page/OpenAccount.jsx
@@ -1,7 +1,19 @@
 function OpenAccount() {
     return ( 
         <>
-            <h1>Open Account</h1>
+            <div className="container p-5 mb-5">
+                <div className="row text-center">
+                    <h1 className="mt-5">
+                        Open An Account in Minutes
+                    </h1>
+                    <p>
+                        Get started with your investment journey in just a few minutes.
+                    </p>
+                    <button style={{ width: "20%", margin: "0 auto" }} className="btn btn-primary p-2 fs-5 mb-5">
+                        Get Started
+                    </button>
+                </div>
+            </div>
         </>
     );
 }
