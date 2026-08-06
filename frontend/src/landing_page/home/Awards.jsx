a3e2e5b342cb6b7421a0c556db1d2019930ad46e
Saumya Khobragade <khobragadesaumya@gmail.com>
2026-08-06 05:46:53 +0530

    Add Hero and Award Sections

--- frontend/src/landing_page/home/Awards.jsx
+++ frontend/src/landing_page/home/Awards.jsx
@@ -1,8 +1,33 @@
 function Awards() {
     return ( 
-        <>
-            <h1>Awards</h1>
-        </>
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-6 p-5">
+                    <img src="/assets/largestBroker.svg" alt="Awards" className="img-fluid" />
+                </div>
+                <div className="col-6 p-5 mt-3">
+                    <h1>Largest Broker in the Country</h1>
+                    <p className="mb-5">2+ million clients contribute to over 15% of all retail order volumes in our market by trading and investing in:</p>
+                    <div className="row">
+                        <div className="col-6">
+                            <ul>
+                                <li>Futures and Options</li>
+                                <li>Commodity Derivatives</li>
+                                <li>Currency Derivatives</li>
+                            </ul>
+                        </div>
+                        <div className="col-6">
+                            <ul>
+                                <li>Stocks & IPOs</li>
+                                <li>Bonds and Government Securities</li>
+                                <li>Mutual Funds</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <img src="/assets/pressLogos.png" alt="Awards" style={{ width: "90%" }} />
+                </div>
+            </div>
+        </div>
     );
 }
 
--- frontend/src/landing_page/home/Hero.jsx
+++ frontend/src/landing_page/home/Hero.jsx
@@ -1,8 +1,19 @@
 function Hero() {
-    return ( 
-        <>
-            <h1>Hero</h1>
-        </>
+    return (
+        <div className="container p-5 mb-5">
+            <div className="row text-center">
+                <img src="/assets/homeHero.png" alt="Hero" className="mb-5" />
+                <h1 className="mt-5">
+                    Invest in Everything You Want, All in One Place
+                </h1>
+                <p>
+                    Invest in everything you want, from stocks to crypto, all in one place.
+                </p>
+                <button style={{ width: "20%", margin: "0 auto" }} className="btn btn-primary p-2 fs-5 mb-5">
+                    Get Started
+                </button>
+            </div>
+        </div>
     );
 }
 
